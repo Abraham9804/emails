2c8be08c6debc8861d81567b04d1c9900c718e78
Abraham Romero <romero9804@hotmail.com>
2024-03-23 14:44:51 -0700

    creacion de funciones para capturar el valor de los inputs

--- js/app.js
+++ js/app.js
@@ -2,5 +2,15 @@ document.addEventListener('DOMContentLoaded', function(){
     const inputEmail = document.querySelector('#email')
     const inputAsunto = document.querySelector('#asunto')
     const inputMensaje = document.querySelector('#mensaje')
-    console.log(inputMensaje)
+    
+
+    inputEmail.addEventListener('blur', validar)
+
+    inputAsunto.addEventListener('blur', validar)
+
+    inputMensaje.addEventListener('blur', validar)
+
+    function validar(e){
+        console.log(e.target.value)
+    }
 })
