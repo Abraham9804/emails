d4466c9d19818836e4748cc9c96312655a055195
Abraham Romero <romero9804@hotmail.com>
2024-03-23 18:39:03 -0700

    creacion de funcion limpiarAlerta

--- js/app.js
+++ js/app.js
@@ -1,25 +1,30 @@
 document.addEventListener('DOMContentLoaded', function(){
+    //definicion de variables para inputs
     const inputEmail = document.querySelector('#email')
     const inputAsunto = document.querySelector('#asunto')
     const inputMensaje = document.querySelector('#mensaje')
     const formulario = document.querySelector('#formulario')
 
+    //eventos sobre inputs
     inputEmail.addEventListener('blur', validar)
-
     inputAsunto.addEventListener('blur', validar)
-
     inputMensaje.addEventListener('blur', validar)
 
+
     function validar(e){
+        const posicion = e.target.parentElement //se toma el div del input que activa el evento
         if(e.target.value.trim() === ''){
-            const posicion = e.target.parentElement
             mostrarAlerta(`el campo ${e.target.id} esta vacio`, posicion)
         }else{
-            console.log('hay algo')
+            limpiarAlerta(posicion)
         }
     }
 
     function mostrarAlerta(mensaje, posicion){
+        //funcion para mostrar solo una alerta por input
+        limpiarAlerta(posicion)
+
+        //funcion para generar la alerta en el input vacio
         const alerta = document.createElement('P')
         alerta.textContent = mensaje
         alerta.classList.add('bg-red-600','text-white','p-2', 'text-center')
@@ -28,7 +33,14 @@ document.addEventListener('DOMContentLoaded', function(){
     }
 
 
-
+    function limpiarAlerta(posicion){
+       const  alerta = posicion.querySelector('.bg-red-600')
+        if(alerta){
+            alerta.remove()
+            console.log(alerta)
+        }
+        
+    }
 
 
 
